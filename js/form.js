5dfde04b1cba7705bf045b2697b88e9834916ce9
Edwyn Guzman <edwynjesus30@gmail.com>
2023-07-28 23:15:21 -0400

    form_ready

--- js/form.js
+++ js/form.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const formulario = document.getElementById("miFormulario");
-const inputName = document.querySelector('input[type="name"]');
-const inputLastName = document.querySelector('input[type="lastname"]');
-const inputEmail = document.querySelector('input[type="email"]');
-const inputSuject = document.querySelector('input[type="Asunto"]');
-const inputMessage = document.querySelector('input[type="message"]');
+const inputName = document.querySelector('input[name="name"]');
+const inputLastName = document.querySelector('input[name="lastname"]');
+const inputEmail = document.querySelector('input[name="email"]');
+const inputSuject = document.querySelector('input[name="Asunto"]');
+const inputMessage = document.querySelector('textarea[name="message"]');
 
 formulario.addEventListener("submit",(e)=>{
-  e.preventDefault();
-   messageSend("edwyn","messs","edwynjesus30@gmail.com")
+    e.preventDefault();
+    let arrInput= [inputName,inputLastName,inputMessage,inputEmail,inputSuject]
+    let datos=[];
+    arrInput.forEach((element,i) => {
+        datos[i] =element.value;
+        element.value=""; 
+    });
+    messageSend(...datos);
     
 })
 
@@ -30,7 +36,29 @@ const messageSend= (name,lname,mg,email,subject="¡Nuevo envío!")=> {
         })
     })
     .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+    .then(data => sendNotification(data.message))
+    .catch(error => sendNotification(error.message,false));
     
    }
+
+  
+
+const sendNotification= (mg,bool=true) =>{
+    let div = document.createElement("DIV");
+    div.classList.add("notification");
+
+    let color= (bool)?"ico_nt":"icon_nt-not";
+    let content = `
+                    <div class="dt_nf">
+                        <h1 class="text_nt">${mg}</h1>
+                    </div>
+                    <div class="${color}">
+                        <i class="fa-solid fa-envelopes-bulk"></i>
+                    </div>
+                    `;
+    div.innerHTML=content;
+    document.body.appendChild(div);
+    setTimeout(()=>{
+        document.querySelector(".notification").remove();
+    },5000)
+}
