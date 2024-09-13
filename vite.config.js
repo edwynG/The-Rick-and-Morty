c4d1187eada403354e8a618e3d358adf40cb5a1e
Edwyn Guzman <edwynjesus30@gmail.com>
2024-09-13 01:18:45 -0400

    bug fix in in deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/The-Rick-and-Morty/" // descomentado solo para deplegue
+  // base:"https://github.com/edwynG/The-Rick-and-Morty" // descomentado solo para deplegue
 })
